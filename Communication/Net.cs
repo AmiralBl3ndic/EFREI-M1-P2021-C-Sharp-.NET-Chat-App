c25418836c60dc8c9465670eceb336ea42bcb8bb
Camille Briand <camille.briand@efrei.net>
2019-12-05 14:34:01 +0100

    Implement boilerplate code for Net.cs

--- Communication/Net.cs
+++ Communication/Net.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Communication
 {
-    public class Net
-    {
-        
-    }
+	public class Net
+	{
+		/// <summary>
+		/// Send a message on a stream by serializing it to binary before doing so
+		/// </summary>
+		/// <param name="stream">Stream to serialize the message on</param>
+		/// <param name="message">Message to send</param>
+		public static void sendMessage(Stream stream, Object message)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			binaryFormatter.Serialize(stream, message);
+		}
+
+		/// <summary>
+		/// Receive a message on a stream by deserializing its binary content
+		/// </summary>
+		/// <param name="stream">Stream to deserialize messages from</param>
+		/// <returns>Deserialized message</returns>
+		public static Object receiveMessage(Stream stream)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			return (Object) binaryFormatter.Deserialize(stream);
+		}
+	}
 }
