1f46e94362634141deb678de19f3f902f1f1bdfc
Camille Briand <camille.briand@efrei.net>
2019-12-05 14:26:27 +0100

    Multiple tries to setup

---
+++ ChatAppClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatAppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Communication/Net.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Communication
+{
+    public class Net
+    {
+        
+    }
+}
