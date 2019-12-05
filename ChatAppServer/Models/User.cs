b269b37abb44a5a639175d348c349b8c002cb54f
Camille Briand <camille.briand@efrei.net>
2019-12-05 22:57:22 +0100

    Implement User class and UserService

---
+++ ChatAppServer/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChatAppServer.Models
+{
+	public class User
+	{
+		[BsonId] [BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		
+		[BsonElement("Username")]
+		public string Username { get; set; }
+
+		[BsonElement("Password")] private string _password;
+		
+		public string Password
+		{
+			get => _password;
+			set => _password = BCrypt.Net.BCrypt.HashPassword(value, workFactor: 12);
+		}
+	}
+}
--- ChatAppServer/Program.cs
+++ ChatAppServer/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using ChatAppServer.Models;
+using ChatAppServer.Services;
 using MongoDB.Driver;
 
 namespace ChatAppServer
@@ -9,38 +8,14 @@ namespace ChatAppServer
 	{
 		static void Main(string[] args)
 		{
-			const string connectionString = "mongodb://localhost:27017";
+			const string connectionString = "mongodb+srv://cs_dotnet_project:EFREI-2021-SE-4LIFE@efrei-m1-2021-nrato.gcp.mongodb.net/test?retryWrites=true&w=majority";
 			var mongoClient = new MongoClient(connectionString);
 
 			IMongoDatabase db = mongoClient.GetDatabase("testDB");
-			IMongoCollection<User> usersCollection = db.GetCollection<User>("usersCollection");
 
-			var filter = new BsonDocument
-			{
-				{"username", "admin"}
-			};
-
-			var userFilter = new User {Username = "admin"};
-
-			usersCollection.FindSync<User>(record => true).ForEachAsync(Console.WriteLine);
-		}
-	}
-
-
-	class User
-	{
-		[BsonId] [BsonRepresentation(BsonType.ObjectId)]
-		public string Id { get; set; }
-		
-		[BsonElement("username")]
-		public string Username { get; set; }
-		
-		[BsonElement("password")]
-		public string Password { get; set; }
-
-		public override string ToString()
-		{
-			return $"User {Id}(Username: {Username}, Password: {Password})";
+			UserService.UsersCollection = db.GetCollection<User>("Users");
+			
+			
 		}
 	}
 }
