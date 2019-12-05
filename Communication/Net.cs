9b155df82bf9e4e3e1766f8abea1f72753907dc6
Camille Briand <camille.briand@efrei.net>
2019-12-05 15:57:50 +0100

    Add AuthenticatedMessage handlers to Net class

--- Communication/Net.cs
+++ Communication/Net.cs
@@ -11,7 +11,7 @@ namespace Communication
 		/// </summary>
 		/// <param name="stream">Stream to serialize the message on</param>
 		/// <param name="message">Message to send</param>
-		public static void sendMessage(Stream stream, Object message)
+		public static void SendMessage(Stream stream, Message message)
 		{
 			BinaryFormatter binaryFormatter = new BinaryFormatter();
 			binaryFormatter.Serialize(stream, message);
@@ -22,10 +22,32 @@ namespace Communication
 		/// </summary>
 		/// <param name="stream">Stream to deserialize messages from</param>
 		/// <returns>Deserialized message</returns>
-		public static Object receiveMessage(Stream stream)
+		public static Message ReceiveMessage(Stream stream)
 		{
 			BinaryFormatter binaryFormatter = new BinaryFormatter();
-			return (Object) binaryFormatter.Deserialize(stream);
+			return (Message) binaryFormatter.Deserialize(stream);
+		}
+
+		/// <summary>
+		/// Send a message with authentication data (user password) by serializing it to binary before doing so
+		/// </summary>
+		/// <param name="stream">Stream to serialize the message on</param>
+		/// <param name="message">Message to send</param>
+		public static void SendAuthenticatedMessage(Stream stream, AuthenticatedMessage message)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			binaryFormatter.Serialize(stream, message);
+		}
+
+		/// <summary>
+		/// Receive an authenticated message on a stream by deserializing its binary content
+		/// </summary>
+		/// <param name="stream">Stream to deserialize messages from</param>
+		/// <returns>Deserialized AuthenticatedMessage</returns>
+		public static AuthenticatedMessage ReceiveAuthenticatedMessage(Stream stream)
+		{
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			return (AuthenticatedMessage) binaryFormatter.Deserialize(stream);
 		}
 	}
 }
