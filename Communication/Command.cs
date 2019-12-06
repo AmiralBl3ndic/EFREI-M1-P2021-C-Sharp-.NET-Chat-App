11bb6ae1fa074c9b9b5f9a5e036975d33de57797
Camille Briand <camille.briand@efrei.net>
2019-12-06 11:30:15 +0100

    Add Command class with Command.Prepare method

---
+++ Communication/Command.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Communication
+{
+	public class Command
+	{
+		/// <summary>
+		/// Dictionary mapping valid command names to their minimum number of arguments
+		/// </summary>
+		public static readonly Dictionary<string, int> ValidCommands = new Dictionary<string, int>()
+		{
+			{"help", 0},          // help
+			{"login", 2},         // login {username} {password}
+			{"register", 2},      // register {username} {password}
+			{"list-topics", 0},   // list-topics
+			{"create-topic", 1},  // create-topic {topicName}
+			{"join", 1},          // join {topicName}
+			{"leave", 1},         // leave {topicName}
+			{"say", 2},           // say {topicName} {messageContent}
+			{"mp", 2},            // mp {userName} {messageContent}
+			{"logout", 0}         // logout
+		};
+
+		public string Name { get; set; }
+		
+		public string[] Arguments { get; set; }
+
+
+		public static Command Prepare(string input)
+		{
+			if (input.Length == 0)  // No need to perform preparation if no input 
+			{
+				return null;
+			}
+			
+			string[] parts = input.Split(" ");  // Split input by spaces
+
+			
+			if (!ValidCommands.Keys.Contains(parts[0]))  // Check if command is known
+			{
+				return null;
+			}
+
+			if (parts.Length - 1 < ValidCommands[parts[0]])  // Check if enough arguments were provided
+			{
+				return null;
+			}
+			
+			// Now, we just have to build the command, the command is valid and has enough arguments 
+
+			var command = new Command {Name = parts[0], Arguments = new string[ValidCommands[parts[0]]]};
+			
+			// Get all needed arguments
+			var i = 1;
+			for (; i <= ValidCommands[command.Name] - 1; i++)
+			{
+				command.Arguments[i - 1] = parts[i];
+			}
+			
+			// Merge remaining parts as the last argument
+			var remainingParts = new ArraySegment<string>(parts, i, parts.Length - 1 - i);
+			command.Arguments[i] = string.Join(" ", remainingParts);
+			
+			return command;
+		}
+	}
+}
