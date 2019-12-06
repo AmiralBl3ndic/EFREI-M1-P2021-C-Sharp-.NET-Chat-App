5d09a0ef489c05b15332d9c9bc312c14b956d0a6
Camille Briand <camille.briand@efrei.net>
2019-12-06 10:13:05 +0100

    Add AuthenticationService.CreateUser

--- ChatAppServer/Services/AuthenticationService.cs
+++ ChatAppServer/Services/AuthenticationService.cs
@@ -22,5 +22,25 @@ namespace ChatAppServer.Services
 
 			return BCrypt.Net.BCrypt.Verify(password, userRecord.Password) ? userRecord : null;
 		}
+
+		/// <summary>
+		/// Create a user database record if the specified username doesn't already exist 
+		/// </summary>
+		/// <param name="username">Username of the user to create</param>
+		/// <param name="password">Password of the user to create</param>
+		/// <returns>User object representing the newly created record if success, null otherwise</returns>
+		public static User CreateUser(string username, string password)
+		{
+			// Check if a user already exists with the specified username
+			if (UserService.GetByUsername(username) != null)
+			{
+				return null;
+			}
+			
+			var newUser = new User{Username = username, Password = password};
+			newUser.HashPassword();
+
+			return UserService.Create(newUser);
+		}
 	}
 }
