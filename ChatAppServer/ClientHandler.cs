c643c6b2eafe534cae38f6991a03b57b22ca695f
Camille Briand <camille.briand@efrei.net>
2019-12-06 10:31:16 +0100

    Add boilerplate code of ClientHandler

---
+++ ChatAppServer/ClientHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatAppServer
+{
+	/// <summary>
+	/// Class handling logic for interacting with a client, meant to be instantiated and execute execute Payload as a Thread
+	/// </summary>
+	public class ClientHandler
+	{
+		/// <summary>
+		/// TCP client the client uses to communicate
+		/// </summary>
+		private TcpClient _tcpClient;
+
+		/// <summary>
+		/// Create an instance of ClientHandler
+		/// </summary>
+		/// <param name="client">TCP client to use for communicating with client</param>
+		public ClientHandler(TcpClient client)
+		{
+			_tcpClient = _tcpClient;
+		}
+
+
+		/// <summary>
+		/// Payload of the thread
+		/// </summary>
+		public void Payload()
+		{
+			Console.WriteLine("ClientHandler thread started");
+
+			// Loop forever (do not lose connection with client)
+			while (true)
+			{
+				Thread.Sleep(10000);  // TODO: implement logic here
+			}
+		}
+	}
+}
