6d551269ea0e7650bd750c8345302242425132a7
Camille Briand <camille.briand@efrei.net>
2019-12-07 17:57:02 +0100

    Add simple loop to produce periodic messages

--- GUI Client/MainWindow.xaml.cs
+++ GUI Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using Communication;
@@ -19,11 +20,18 @@ namespace GUI_Client
 			DataContext = this;
 			
 			_tcpClient = new TcpClient(FindResource("ServerIp").ToString(), Int32.Parse(FindResource("ServerPort").ToString()));
+
+
+			var serverListenerThread = new Thread(ListenToServer);
+			serverListenerThread.SetApartmentState(ApartmentState.STA);
+			serverListenerThread.IsBackground = true;
+			serverListenerThread.Start();
 		}
 
 		private static void SubmitCommand(string commandText)
 		{
 			var command = Command.Prepare(commandText);
+			Console.WriteLine(command);
 		}
 
 		private void UserInput_OnKeyDown(object sender, KeyEventArgs key)
@@ -31,17 +39,23 @@ namespace GUI_Client
 			// Process only if current input is at least 2 characters long
 			if (key.Key != Key.Enter || UserInputField.Text.Length < 2) return;
 			
-			SubmitCommand(UserInputField.Text);
-			UserInputField.Text = "";
+			Dispatcher.Invoke(() =>
+			{
+				SubmitCommand(UserInputField.Text);
+				UserInputField.Text = "";
+			});
 		}
 		
 		private void UserInput_OnSubmit(object sender, RoutedEventArgs e)
 		{
 			// Process only if current input is at least 2 characters long
 			if (UserInputField.Text.Length < 2) return;
-			
-			SubmitCommand(UserInputField.Text);
-			UserInputField.Text = "";
+
+			Dispatcher.Invoke(() =>
+			{
+				SubmitCommand(UserInputField.Text);
+				UserInputField.Text = "";
+			});
 		}
 	}
 }
---
+++ GUI Client/MainWindowServerListener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace GUI_Client
+{
+	public partial class MainWindow : Window
+	{
+		private void ListenToServer()
+		{
+			Console.Write("Begin...");
+			
+			Console.WriteLine("end");
+
+			Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+			{
+				var connectedMessage = new TextBlock() {Text = $"Connected to server {(FindResource("ServerIp"))}:{(FindResource("ServerPort"))}"};
+				connectedMessage.FontStyle = FontStyles.Italic;
+				MessagesPanel.Children.Add(connectedMessage);
+			}));
+			
+			while (true)
+			{
+				Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+				{
+					var sampleMessage = new TextBlock() {Text = "I am a sample message"};
+					MessagesPanel.Children.Add(sampleMessage);
+				}));
+				
+				Thread.Sleep(1500);
+			}
+		}
+	}
+}
