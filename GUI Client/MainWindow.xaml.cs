8794b51cfdf5287d886b4358fcea86bfea692ed2
Camille Briand <camille.briand@efrei.net>
2019-12-07 16:28:21 +0100

    Improve GUI and connection stability

--- GUI Client/MainWindow.xaml.cs
+++ GUI Client/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Input;
+using Communication;
 
 namespace GUI_Client
 {
@@ -10,30 +11,35 @@ namespace GUI_Client
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private TcpClient _tcpClient = null;
+		
 		public MainWindow()
 		{
 			InitializeComponent();
 			DataContext = this;
 			
-			TcpClient tcpClient = new TcpClient("127.0.0.1", 4321);
+			_tcpClient = new TcpClient(FindResource("ServerIp").ToString(), Int32.Parse(FindResource("ServerPort").ToString()));
 		}
 
-		private void SubmitCommand(string commandText)
+		private static void SubmitCommand(string commandText)
 		{
-			Console.WriteLine("Submitted: " + commandText);
+			var command = Command.Prepare(commandText);
 		}
 
 		private void UserInput_OnKeyDown(object sender, KeyEventArgs key)
 		{
-			if (key.Key == Key.Enter)
-			{
-				SubmitCommand(UserInputField.Text);
-				UserInputField.Text = "";
-			}
+			// Process only if current input is at least 2 characters long
+			if (key.Key != Key.Enter || UserInputField.Text.Length < 2) return;
+			
+			SubmitCommand(UserInputField.Text);
+			UserInputField.Text = "";
 		}
 		
 		private void UserInput_OnSubmit(object sender, RoutedEventArgs e)
 		{
+			// Process only if current input is at least 2 characters long
+			if (UserInputField.Text.Length < 2) return;
+			
 			SubmitCommand(UserInputField.Text);
 			UserInputField.Text = "";
 		}
