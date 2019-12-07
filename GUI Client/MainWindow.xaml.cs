6207dcd47fe42c99be704154d948014677bbdc91
Camille Briand <camille.briand@efrei.net>
2019-12-07 19:51:58 +0100

    Add basic command filtering before sending

--- GUI Client/MainWindow.xaml.cs
+++ GUI Client/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using Communication;
 
 namespace GUI_Client
@@ -28,10 +29,56 @@ namespace GUI_Client
 			serverListenerThread.Start();
 		}
 
-		private static void SubmitCommand(string commandText)
+		private void SubmitCommand(string commandText)
 		{
 			var command = Command.Prepare(commandText);
-			Console.WriteLine(command);
+
+			if (command == null)
+			{
+				DisplayError("Invalid command");
+				return;
+			}
+
+			if (command.Error != null)
+			{
+				DisplayError(command.Error);
+				return;
+			}
+
+			if (command.Name != "help")
+			{
+				Net.SendCommand(_tcpClient.GetStream(), command);
+			}
+			else
+			{
+				DisplayHelpDialog();
+			}
+		}
+		
+		private static void DisplayHelpDialog()
+		{
+			MessageBox.Show(
+"Available commands:\n\n" +
+	        "help:  Displays this window\n" +
+	        "login {username} {password}:  Log into your account\n" +
+	        "register {username} {password}:  Create an account\n" +
+	        "list-topics:  Get the list of all topics\n" +
+	        "create-topic:  Create a topic\n" +
+	        "join {topic-name}:  Join a topic\n" +
+	        "leave {topic-name}:  Leave a topic\n" +
+	        "say {topic} {message}:  Say something in a topic\n" +
+	        "dm {username} {message}:  Send a private message to the specified user\n" +
+	        "logout:  Disconnect from your account",
+				"Help",
+				MessageBoxButton.OK,
+				MessageBoxImage.Information);
+		}
+
+
+		private void DisplayError(string error)
+		{
+			var errorBlock = new TextBlock() {Text = error, Foreground = Brushes.DarkRed};
+			MessagesPanel.Children.Add(errorBlock);
 		}
 	}
 }
