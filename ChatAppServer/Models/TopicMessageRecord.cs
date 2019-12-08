93eb0d7995dcad1eb9c24c9bb77eb59accda060b
Camille Briand <camille.briand@efrei.net>
2019-12-08 17:29:31 +0100

    Add TopicMessageRecord model

---
+++ ChatAppServer/Models/TopicMessageRecord.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChatAppServer.Models
+{
+	public class TopicMessageRecord
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+		
+		[BsonElement("Topic")]
+		public string Topic { get; set; }
+		
+		[BsonElement("Username")]
+		public string Username { get; set; }
+	}
+}
