39b181f0296a85dce67aa107d02ce651531b6629
Camille Briand <camille.briand@efrei.net>
2019-12-08 17:06:41 +0100

    Add base Topic model

---
+++ ChatAppServer/Models/Topic.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChatAppServer.Models
+{
+	public class Topic
+	{
+		[BsonId] [BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		
+		[BsonElement("Name")]
+		public string Name { get; set; }
+	}
+}
