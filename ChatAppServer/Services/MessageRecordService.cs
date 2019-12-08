904aad8aef85e7099208f971968282c425244448
Camille Briand <camille.briand@efrei.net>
2019-12-08 18:56:59 +0100

    Add methods for private messages to MessageRecordService

--- ChatAppServer/Services/MessageRecordService.cs
+++ ChatAppServer/Services/MessageRecordService.cs
@@ -10,11 +10,35 @@ namespace ChatAppServer.Services
 		
 		public static IMongoCollection<PrivateMessageRecord> PrivateMessagesCollection { get; set; }
 		
+		/// <summary>
+		/// Get the list of unread private messages of a given user
+		/// </summary>
+		/// <param name="user">User to get the list of messages for</param>
+		/// <returns>List of unread messages for passed user</returns>
 		public List<PrivateMessageRecord> GetPrivateMessages(User user)
 		{
 			return PrivateMessagesCollection
 				.Find(record => record.Receiver == user.Username)
 				.ToList();
+		}
+		
+		/// <summary>
+		/// Clear the private messages of a given user
+		/// </summary>
+		/// <param name="user">User to clear the private messages of</param>
+		public void ClearPrivateMessages(User user)
+		{
+			PrivateMessagesCollection.DeleteMany(record => record.Receiver == user.Username);
+		}
+
+
+		/// <summary>
+		/// Create a private message record
+		/// </summary>
+		/// <param name="privateMessageRecord">Record to insert in the database</param>
+		public void CreatePrivateMessage(PrivateMessageRecord privateMessageRecord)
+		{
+			PrivateMessagesCollection.InsertOne(privateMessageRecord);
 		}
 	}
 }
