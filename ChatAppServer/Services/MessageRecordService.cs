2a1c22de6a910d09fec3474cd12bfa09af9bac27
Camille Briand <camille.briand@efrei.net>
2019-12-08 17:44:11 +0100

    Add very basic MessageRecordService.GetPrivateMessages method

--- ChatAppServer/Services/MessageRecordService.cs
+++ ChatAppServer/Services/MessageRecordService.cs
@@ -1,4 +1,5 @@
-﻿using ChatAppServer.Models;
+﻿using System.Collections.Generic;
+using ChatAppServer.Models;
 using MongoDB.Driver;
 
 namespace ChatAppServer.Services
@@ -8,5 +9,13 @@ namespace ChatAppServer.Services
 		public static IMongoCollection<TopicMessageRecord> TopicMessagesCollection { get; set; }
 		
 		public static IMongoCollection<PrivateMessageRecord> PrivateMessagesCollection { get; set; }
+
+
+		public List<PrivateMessageRecord> GetPrivateMessages(User user)
+		{
+			return PrivateMessagesCollection
+							.Find(record => record.Receiver == user.Username)
+							.ToList();
+		}
 	}
 }
