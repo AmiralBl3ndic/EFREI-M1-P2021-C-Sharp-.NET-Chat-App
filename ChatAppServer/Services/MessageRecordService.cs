f419806288b2241d7c6116698dfd19e1d5f82f5c
Camille Briand <camille.briand@efrei.net>
2019-12-08 17:30:28 +0100

    Add basic boilerplate for MessageRecordService

---
+++ ChatAppServer/Services/MessageRecordService.cs
@@ -0,0 +1,10 @@
+﻿using ChatAppServer.Models;
+using MongoDB.Driver;
+
+namespace ChatAppServer.Services
+{
+	public class MessageRecordService
+	{
+		public IMongoCollection<TopicMessageRecord> TopicMessagesCollection { get; set; }
+	}
+}
